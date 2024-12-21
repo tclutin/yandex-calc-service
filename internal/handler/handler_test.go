ceb67b4cd9eef62df0cecb2ccd664b3e15a82a02
Lutin <tclutin01@gmail.com>
2024-12-21 23:10:19 +0500

    add: test for valid json schema

--- internal/handler/handler_test.go
+++ internal/handler/handler_test.go
@@ -27,7 +27,7 @@ func TestHandler_Success(t *testing.T) {
 	}
 }
 
-func TestHandler_MethodNowAllowed(t *testing.T) {
+func TestHandler_MethodNotAllowed(t *testing.T) {
 	data := `{"expression": "2+2"}`
 
 	h := New(logger.New(), calc.New())
@@ -41,5 +41,39 @@ func TestHandler_MethodNowAllowed(t *testing.T) {
 
 	if status := recorder.Code; status != http.StatusMethodNotAllowed {
 		t.Errorf("expected status 405, got %v", status)
+	}
+}
+
+func TestHandler_ValidJsonBody(t *testing.T) {
+	data := `{"expression":}`
+
+	h := New(logger.New(), calc.New())
+
+	req := httptest.NewRequest("POST", "/api/v1/calculate", bytes.NewBufferString(data))
+	req.Header.Set("Content-Type", "application/json")
+
+	recorder := httptest.NewRecorder()
+
+	h.Init().ServeHTTP(recorder, req)
+
+	if status := recorder.Code; status != http.StatusBadRequest {
+		t.Errorf("expected status 400, got %v", status)
+	}
+}
+
+func TestHandler_RequiredJsonField(t *testing.T) {
+	data := `{"expressin": "2+2"}`
+
+	h := New(logger.New(), calc.New())
+
+	req := httptest.NewRequest("POST", "/api/v1/calculate", bytes.NewBufferString(data))
+	req.Header.Set("Content-Type", "application/json")
+
+	recorder := httptest.NewRecorder()
+
+	h.Init().ServeHTTP(recorder, req)
+
+	if status := recorder.Code; status != http.StatusBadRequest {
+		t.Errorf("expected status 400, got %v", status)
 	}
 }
